686b4873afe34a09f91b49bc077813c968c90538
Sondos-maher <gweilysondos@gmail.com>
2026-08-18 16:12:41 +0300

    edited

--- lib/welcome-screen.dart
+++ lib/welcome-screen.dart
@@ -5,35 +5,75 @@ class welcomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar() ,
-      body:Column(
-        children: [
-          Container(
-            padding: EdgeInsetsDirectional.all(10),
-
-            decoration: BoxDecoration(color: Color(0xff7876CD), borderRadius: BorderRadius.circular(25) ),
-            child: Column(children: [
-              Text("Know Your Body Better ,Get Your BMI Score in Less Than a Minute!" ,style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700,color: Colors.white),),
-              SizedBox(height: 10,),
-              Text("Know Your Body Better ,Get Your BMI Score in Less Than a Minute!It takes just 30 seconds – and your health is worth it ,Get Start" ,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600,color: Colors.white),),
-              SizedBox(height: 20,),
-              Divider(),
-
-              SizedBox(
-
-                  child:
-                  ElevatedButton( style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9)), backgroundColor: Color(0xff484783) ),
-                      onPressed: (){}, child: Text("Get Started" ,style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700,color: Colors.white),))),
-            ], ),
-
-          ) ,
-
-
-
-
-
-        ],) ,
-      
-    );
-  }
+appBar: AppBar(),
+body: Column(
+mainAxisAlignment: MainAxisAlignment.end,
+children: [
+Image.asset("assets/first_img.png"),
+// image
+Container(
+padding: EdgeInsets.symmetric(horizontal: 15, vertical: 45),
+height: 400,
+decoration: BoxDecoration(
+color: Color(0xff7876CD),
+borderRadius: BorderRadius.circular(25),
+),
+child: Column(
+children: [
+Text(
+"Know Your Body Better ,Get Your BMI Score in Less Than a Minute!",
+style: TextStyle(
+fontWeight: FontWeight.w700,
+fontSize: 24,
+color: Colors.white,
+),
+),
+SizedBox(height: 24),
+Text(
+"It takes just 30 seconds – and your health is worth it!",
+style: TextStyle(
+fontSize: 16,
+fontWeight: FontWeight.w400,
+color: Color(0xffF8F9FFE5),
+),
+),
+SizedBox(height: 24),
+Divider(),
+SizedBox(height: 24),
+SizedBox(
+width: 390,
+child: ElevatedButton(
+style: ElevatedButton.styleFrom(
+shape: RoundedRectangleBorder(
+borderRadius: BorderRadius.circular(9),
+),
+backgroundColor: Color(0xff484783),
+),
+onPressed: () {
+onGetStarted(context);
+},
+child: Text(
+"Get Started",
+style: TextStyle(
+fontWeight: FontWeight.w700,
+fontSize: 16,
+color: Colors.white,
+),
+),
+),
+),
+SizedBox(height: 30),
+],
+),
+),
+],
+),
+);
 }
+
+void onGetStarted(BuildContext context) {
+// push
+Navigator.push(
+context,
+MaterialPageRoute(builder: (context) => CalculationScreen()),
+);
