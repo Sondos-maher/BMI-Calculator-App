7906d9a4545b9ee0ab49332e9287e1d770882f31
Sondos-maher <gweilysondos@gmail.com>
2026-08-23 17:26:27 +0300

    second_page.

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,22 @@ import 'package:bmi_aug/welcome-screen.dart';
 import 'package:dio/dio.dart' show Dio;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 void main() async {
-  // runApp(MyApp());
   var apiobject = Dio();
   var link = "https://jsonplaceholder.typicode.com/users";
   var res = await apiobject.get(link);
   print(res);
   print("################################# create user");
-  var body={
-    {
-      "name": "Sondos Maher",
-      "username": "SM",
-      "email": "Sondos@nti.com"
-    }
-
+  var body = {
+    "name": "Sondos Maher",
+    "username": "SM",
+    "email": "Sondos@nti.com"
   };
   var link2 = "https://jsonplaceholder.typicode.com/users";
-  var res2 = await apiobject.post(link2,data: body);
-  print (res2);
-
-
+  var res2 = await apiobject.post(link2, data: body);
+  print(res2);
 }
-
-
 
 
 
